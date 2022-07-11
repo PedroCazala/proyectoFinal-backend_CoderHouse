4689e9f5ac8974ce84f68cdda315a2093a1d584c
Pedro Cazala <cazalapedro@gmail.com>
2022-07-11 20:55:44 -0300

    PRUEBA 2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express'
-// import { allRoutes } from './src/routes/allRoutes.js'
+import { allRoutes } from './src/routes/allRoutes.js'
 // import 'dotenv/config'
-// import session  from 'express-session'
+import session  from 'express-session'
 
-// import passport from 'passport'
+import passport from 'passport'
 
 export const app = express()
 const PORT = process.env.PORT || 8000
@@ -14,34 +14,34 @@ const server = app.listen(PORT,()=>{
 })
 server.on('error', error  => console.log(`Error en el servidor ${error}`))
 
-app.get('/',(req,res)=>{
-    res.send('prueba heroku')
-})
+// app.get('/',(req,res)=>{
+//     res.send('prueba heroku')
+// })
 
-// //poder enviar json
-// app.use(express.json())
-// app.use(express.urlencoded({extended:true}))
+//poder enviar json
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-// // -------- ACCESO A ARCHIVOS PUBLICOS -------
-// app.use(express.static('public'))
+// -------- ACCESO A ARCHIVOS PUBLICOS -------
+app.use(express.static('public'))
 
 
 
-// // -------- VISTAS CON EJS -------
-// app.set('view engine','ejs')
-// app.set('views','./src/views')
+// -------- VISTAS CON EJS -------
+app.set('view engine','ejs')
+app.set('views','./src/views')
 
-// // -------- Manejo de sesiones -------
-// app.use(session({
-//     // store: MongoStore,
-//     secret: 'dfafasf',
-//     resave: false,
-//     saveUninitialized:false
-// }))
-// // -------- PARA USAR PASSPORT -------
-// app.use(passport.initialize())                              
-// app.use(passport.session())
-// import './src/passport/passport-local.js'
+// -------- Manejo de sesiones -------
+app.use(session({
+    // store: MongoStore,
+    secret: 'dfafasf',
+    resave: false,
+    saveUninitialized:false
+}))
+// -------- PARA USAR PASSPORT -------
+app.use(passport.initialize())                              
+app.use(passport.session())
+import './src/passport/passport-local.js'
 
-// // -------- ROUTES -------
-// app.use('/',allRoutes)
+// -------- ROUTES -------
+app.use('/',allRoutes)
