6a3e5ff4a95de99d5be4b2928ed8b396c3ed2eba
Pedro Cazala <cazalapedro@gmail.com>
2024-01-31 15:11:24 -0300

    j

--- server.js
+++ server.js
@@ -107,8 +107,10 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(optionsSwagge
 
 // -------- ROUTES (Debe estar al final del server) -------
 app.use('/',allRoutes)
-import serverless from "serverless-http";
-export const handler = serverless(app);
+
+
+// import serverless from "serverless-http";
+// export const handler = serverless(app);
 
 
 // process.on('uncaughtException', (error, source) => {
